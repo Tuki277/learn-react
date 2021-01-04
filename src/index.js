93b4b061ddc55d76abd10ecd21e13b30cf8d1d4f
Tran Hoang Huy <huybeos2707@gmail.com>
2021-01-04 19:45:59 +0700

    learn

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Demo from './Demo'
 import reportWebVitals from './reportWebVitals';
+
+{/* <Demo /> dược gọi là một component, render ra component */}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Demo />
   </React.StrictMode>,
   document.getElementById('root')
+  // root được lấy từ file public/index.html
 );
 
 // If you want to start measuring performance in your app, pass a function
