2b94583c141bbf626e17e7b023c628e6532e5fd0
Tran Hoang Huy <huybeos2707@gmail.com>
2021-01-16 00:33:55 +0700

    add more

--- learn-router/src/App.js
+++ learn-router/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import Home from './Components/Home'
 import About from './Components/About'
 
@@ -12,16 +12,27 @@ class App extends Component {
 
                     <nav className="navbar navbar-inverse">
                         <ul className="nav navbar-nav">
-                            <li className="active">
-                                <Link to='/'>Trang chủ</Link>
+                            <li>
+
+                                {/* Sử dụng NavLink */}
+
+                                {/* <Link to='/'>Trang chủ</Link> */}
+
+                                {/* sử dụng activeStyle */}
+                                {/* <NavLink exact activeStyle = {{ color : 'white' }} to='/'>Trang chủ</NavLink> */}
+
+                                {/* sử dụng activeClass */}
+                                <NavLink activeClassName="active" to='/'>Trang chủ</NavLink>
                             </li>
                             <li>
-                                <Link to='/about'>About</Link>
+
+                                {/* <Link to='/about'>About</Link> */}
+                                <NavLink activeClassName="active" to='/about'>About</NavLink>
                             </li>
                         </ul>
                     </nav>
-                    <Route path = "/" exact  component = { Home } />
-                    <Route path = "/about" component = { About } />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
                 </div>
             </Router>
 
